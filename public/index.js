720688ce2dba1fbcb5c6a845779d0f84302559cb
daltonbrinkley <dbrinkles@gmail.com>
2018-04-30 21:03:17 -0500

    Added Wrapper and Individual Resume Page for Peter

--- public/index.js
+++ public/index.js
@@ -56,8 +56,41 @@ var HomePage = {
   computed: {}
 };
 
+var ResumePage = {
+  template: "#resume-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      students: [
+        {
+          first_name: "Peter",
+          last_name: "Jang",
+          email: "peter@email.com",
+          phone_number: "12121212121",
+          bio: "  ",
+          linkedin_url: "something url",
+          twitter_url: "url",
+          personal_url: " url",
+          resume_url: "url",
+          github_url: "url",
+          photo: "",
+          experience: {},
+          education: {},
+          skills: {}
+        },
+      ]
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
 var router = new VueRouter({
-  routes: [{ path: "/", component: HomePage }],
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/resumepage", component: ResumePage }
+  ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
   }
